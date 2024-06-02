592890b16bf08945b707633bf7d3f47151a7ab48
yangyangli-personal <yangyangli.ca@gmail.com>
2024-06-02 11:03:31 -0400

    coding for week 3 lab

--- lib/main.dart
+++ lib/main.dart
@@ -7,18 +7,16 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(   // Constructor for MaterialApp
-
-      title: 'Flutter Demo',
+    return MaterialApp(
+      title: 'My Recipes App',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'My Recipes App'),
     );
   }
 }
@@ -33,100 +31,68 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-  var isChecked = false;
-  late TextEditingController _controller; // late means initialize later, but not null
-  late TextEditingController _controller2;
-
-  @override
-  void initState() {
-    // initialize object, onloaded in HTML
-    super.initState();
-    _controller = TextEditingController();
-    _controller2 = TextEditingController();
-  }
-
-  @override
-  void dispose() { //unloading the page
-    super.dispose();
-    _controller.dispose(); //delete memory of _controller
-    _controller2.dispose();
-  }
-
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      drawer: Drawer( child:
-      Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [ Text("More items:"),
-        Text("More items 2:"),
-        Text("More items 3:"),
-        Text("More items 4:"),
-        Text("More items 5:"),
-        Text("More items 6:"),
-      ],)
-      ),
       appBar: AppBar(
         backgroundColor: Colors.lightGreen,
-        title: Text("My app is great!"),
-
-        actions: [
-          ElevatedButton(onPressed: (){   }, child: Text("Delete")) ,
-        SizedBox(width:350, height:50),
-        ElevatedButton(onPressed: (){}, child: Image.asset("images/algonquin.jpg", height:50, width: 50,),),
-
-          ElevatedButton(onPressed: (){   }, child: Text("Delete 2")) ,
-          ElevatedButton(onPressed: (){   }, child: Text("Delete")) ,
-        ],
+        title: Text(widget.title),
       ),
-
-      body: Center(
+      body: SingleChildScrollView(
         child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-      mainAxisAlignment: MainAxisAlignment.start,
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: <Widget>[
-          Text("One pan skillet cookie", style: TextStyle(fontSize: 50.0, color: Colors.orange),),
-          Text("INGREDIENT LIST"),
-            SizedBox(height:50),
-            Row( mainAxisAlignment: MainAxisAlignment.start,
+            Text("Delicious Recipes", style: TextStyle(fontSize: 40, fontWeight: FontWeight.bold)),
+            Align(
+              alignment: Alignment.centerLeft,
+              child: Text("Find your favorite recipes", style: TextStyle(fontSize: 20)),
+            ),
+            Text("By Meat", style: TextStyle(fontSize: 24)),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
               children: [
-                Icon(Icons.star),
-              Text("1 STICK UNSALTED BUTTER")
-            ],),
-
-            Row( mainAxisAlignment: MainAxisAlignment.start,
+                Expanded(child: _buildImageStack("images/beef.jpg", "Beef")),
+                Expanded(child: _buildImageStack("images/chicken.jpg", "Chicken")),
+                Expanded(child: _buildImageStack("images/pork.jpg", "Pork")),
+                Expanded(child: _buildImageStack("images/seafood.jpg", "Seafood")),
+              ],
+            ),
+            Text("By Course", style: TextStyle(fontSize: 24)),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
               children: [
-                Icon(Icons.star),
-                Text("1/2 CUP GRANULATED SUGAR")
-              ],),
-
+                Expanded(child: _buildImageStack("images/main_dishes.jpg", "Main Dishes", alignment: Alignment.bottomCenter)),
+                Expanded(child: _buildImageStack("images/salad.jpg", "Salad Recipes", alignment: Alignment.bottomCenter)),
+                Expanded(child: _buildImageStack("images/side_dishes.jpg", "Side Dishes", alignment: Alignment.bottomCenter)),
+                Expanded(child: _buildImageStack("images/crockpot.jpg", "Crockpot", alignment: Alignment.bottomCenter)),
+              ],
+            ),
+            Text("By Dessert", style: TextStyle(fontSize: 24)),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                Expanded(child: _buildImageStack("images/cakes.jpg", "Cakes", alignment: Alignment.bottomCenter)),
+                Expanded(child: _buildImageStack("images/pies.jpg", "Pies", alignment: Alignment.bottomCenter)),
+                Expanded(child: _buildImageStack("images/cookies.jpg", "Cookies", alignment: Alignment.bottomCenter)),
+                Expanded(child: _buildImageStack("images/ice_cream.jpg", "Ice Cream", alignment: Alignment.bottomCenter)),
+              ],
+            ),
           ],
         ),
       ),
-      /*floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ),*/ // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 
-void changeCheckbox(bool ? ch) {
-  if (ch != null) {
-    setState(() {
-      isChecked = ch;
-    });
-  }
-}
-  void buttonClicked(){
-
+  Widget _buildImageStack(String imagePath, String text, {Alignment alignment = Alignment.center}) {
+    return Padding(
+      padding: const EdgeInsets.all(4.0),
+      child: Stack(
+        alignment: alignment,
+        children: [
+          Image.asset(imagePath, fit: BoxFit.cover),
+          Text(text, style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold, shadows: [Shadow(color: Colors.black, blurRadius: 2)])),
+        ],
+      ),
+    );
   }
 }
