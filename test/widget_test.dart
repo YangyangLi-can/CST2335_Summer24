a30940e5497c55240db6aeb5424aca6967ad8150
yangyangli-personal <yangyangli.ca@gmail.com>
2024-07-03 18:15:47 -0400

    comment test code

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -10,21 +10,25 @@ import 'package:flutter_test/flutter_test.dart';
 
 import 'package:cst2335_summer24/main.dart';
 
+
+// Import the generated database file
+import 'package:cst2335_summer24/database.dart';
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(const TodoApp());
-
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
-
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
-
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
-  });
+  // testWidgets('Counter increments smoke test', (WidgetTester tester) async {
+  //   // Build our app and trigger a frame.
+  //   final database = await $FloorAppDatabase.databaseBuilder('app_database.db').build();
+  //   await tester.pumpWidget(const TodoApp(database: database));
+  //
+  //   // Verify that our counter starts at 0.
+  //   expect(find.text('0'), findsOneWidget);
+  //   expect(find.text('1'), findsNothing);
+  //
+  //   // Tap the '+' icon and trigger a frame.
+  //   await tester.tap(find.byIcon(Icons.add));
+  //   await tester.pump();
+  //
+  //   // Verify that our counter has incremented.
+  //   expect(find.text('0'), findsNothing);
+  //   expect(find.text('1'), findsOneWidget);
+  // });
 }
